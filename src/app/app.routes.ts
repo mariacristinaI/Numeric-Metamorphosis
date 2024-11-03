e6dbc83df4d95020acf7a4f8e4615242f0e8291b
Maria-Cristina Iosub <mariacristinaiosub@protonmail.com>
2024-11-03 11:04:15 +0200

    Add focus music component
    Add focus music component with rotating vinyl animation and appropriate music selection.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,19 +9,25 @@ export const routes: Routes = [
   {
     path: 'welcome-screen',
     loadComponent: () => import('./welcome-screen/welcome-screen.component').then(mod => mod.WelcomeScreenComponent),
-    title: '',
+    title: 'Algorithms Playground',
     data: { animation: '' }
   },
   {
     path: 'transform-numbers',
     loadComponent: () => import('./transform-number/transform-number.component').then(mod => mod.TransformNumberComponent),
-    title: '',
+    title: 'Transform Numbers | Algorithms Playground',
     data: { animation: '' }
   },
   {
     path: 'create-a-list',
     loadComponent: () => import('./list/list.component').then(mod => mod.ListComponent),
-    title: '',
+    title: 'Create Lists | Algorithms Playground',
+    data: { animation: '' }
+  },
+  {
+    path: 'play-music',
+    loadComponent: () => import('./play-music/play-music.component').then(mod => mod.PlayMusicComponent),
+    title: 'Playlist | Algorithms Playground',
     data: { animation: '' }
   }
 ];
---
+++ src/app/play-music/play-music.component.ts
@@ -0,0 +1,32 @@
+import { Component, Renderer2} from '@angular/core';
+
+@Component({
+  selector: 'app-play-music',
+  standalone: true,
+  imports: [],
+  templateUrl: './play-music.component.html',
+  styleUrl: './play-music.component.scss',
+})
+export class PlayMusicComponent {
+  private readonly images = [
+    'url("/assets/images/albums/musicforprogramming.png")',
+    'url("/assets/images/albums/Tycho-Dive.jpg")'
+  ];
+  currentIndex = 0;
+  isAnimating = false;
+
+  constructor(private readonly renderer: Renderer2) {}
+
+  changeBackground(div: HTMLDivElement) {
+    this.isAnimating = true;
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+
+    this.renderer.setStyle(div, 'background-image', this.images[this.currentIndex]);
+    this.renderer.addClass(div, 'rotate');
+
+    setTimeout(() => {
+      this.isAnimating = false;
+      this.renderer.removeClass(div, 'rotate');
+    }, 10000000);
+  }
+}
