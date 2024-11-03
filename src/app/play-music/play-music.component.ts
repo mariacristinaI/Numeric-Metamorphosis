d2db36eb265ff7ce3bbc3339a61941f6b7f5b2bc
Maria-Cristina Iosub <mariacristinaiosub@protonmail.com>
2024-11-03 11:41:47 +0200

    Hide vinyl disc
    Commented the code for vinyl disc display and its dynamic interaction with other content (HTML, CSS, JS).

--- src/app/play-music/play-music.component.ts
+++ src/app/play-music/play-music.component.ts
@@ -8,25 +8,30 @@ import { Component, Renderer2} from '@angular/core';
   styleUrl: './play-music.component.scss',
 })
 export class PlayMusicComponent {
-  private readonly images = [
-    'url("/assets/images/albums/musicforprogramming.png")',
-    'url("/assets/images/albums/Tycho-Dive.jpg")'
-  ];
-  currentIndex = 0;
-  isAnimating = false;
+  // private readonly images = [
+  //   'url("/assets/images/albums/musicforprogramming.png")',
+  //   'url("/assets/images/albums/Tycho-Dive.jpg")'
+  // ];
+  // currentIndex = 0;
+  // isAnimating = false;
 
   constructor(private readonly renderer: Renderer2) {}
 
-  changeBackground(div: HTMLDivElement) {
-    this.isAnimating = true;
-    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  // changeBackground(div: HTMLDivElement) {
+  //   this.isAnimating = true;
+  //   this.currentIndex = (this.currentIndex + 1) % this.images.length;
 
-    this.renderer.setStyle(div, 'background-image', this.images[this.currentIndex]);
-    this.renderer.addClass(div, 'rotate');
+  //   this.renderer.setStyle(div, 'background-image', this.images[this.currentIndex]);
+  //   this.renderer.addClass(div, 'rotate');
 
-    setTimeout(() => {
-      this.isAnimating = false;
-      this.renderer.removeClass(div, 'rotate');
-    }, 10000000);
-  }
+  //   setTimeout(() => {
+  //     this.isAnimating = false;
+  //     this.renderer.removeClass(div, 'rotate');
+  //   }, 10000000);
+  // }
+
+  updateMusicText() {
+    document.getElementById('cat-text')!.innerHTML = 
+      '<p>Purrfectly meow-sic for your work day! ♫ ♪ ♫ ♪</p>';
+  }  
 }
